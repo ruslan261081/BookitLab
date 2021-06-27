1c130a1290df7ae958bbd8ddbaaa1bcdc5248f5a
ruslan261081 <45044693+ruslan261081@users.noreply.github.com>
2021-06-27 07:17:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookitLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (BookitLabEntities bookitLab = new BookitLabEntities())
+            {
+                List<Customer> customers = bookitLab.Customers.ToList();
+
+            }
+        }
+    }
+}
